afa792feb81158c5f9f636c6e315dbc85f0aa14d
notjiang <128391769+Hj006@users.noreply.github.com>
2024-10-03 09:49:20 -0500

    Modified the initialization of ElasticNet.py

--- elasticnet/models/ElasticNet.py
+++ elasticnet/models/ElasticNet.py
@@ -1,7 +1,12 @@
 
 
 class ElasticNetModel():
-    def __init__(self):
+    def __init__(self,lambdas,thresh = 0.5,max_iter=1000, tol=1e-4):
+        self.lambdas = lambdas
+        self.thresh = thresh# between [0,1],0 means only L1 regularization, 1 means only L2 regularization      
+        self.max_iter = max_iter#Maximum number of iterations
+        self.tol = tol
+        self.w = None
         pass
 
 
@@ -13,5 +18,5 @@ class ElasticNetModelResults():
     def __init__(self):
         pass
 
-    def predict(self, x):
-        return 0.5
+    def predict(self, X):
+        return X @ self.w 
